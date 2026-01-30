dbf86d89dc7b79ce1644749bf5717f3ff0de48cb
Gustav-Smith <gustavojeanpt@gmail.com>
2026-01-30 20:04:35 -0300

    implementa interação com o usuário para criação de conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,23 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, mundo!");
+        // conhecer e importar a classe Scanner
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        // exibir as mensagens para o nosso usuario
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int numero = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Por favor, digite o nome do Cliente !");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta !");
+        double saldo = Double.parseDouble(scanner.nextLine());
+
+        // exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
